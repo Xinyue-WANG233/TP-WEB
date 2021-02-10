b079e7f620c9f938da2427fb0e011c4559165c5a
Xinyue-WANG233 <78864878+Xinyue-WANG233@users.noreply.github.com>
2021-02-10 19:43:29 +0100

    Update select.js

--- select.js
+++ select.js
@@ -1,9 +1,9 @@
 		function showCity() { 
-  		let nomVilleSelectionnee = document.getElementById("ville-select").value;
-	    let cities = document.getElementsByClassName("city");
+  		var nomVilleSelectionnee = document.getElementById("ville-select").value;
+	        var cities = document.getElementsByClassName("city");
 
-  		 for (i=0; i<l; i++) {
-  		  if (citis[i].id == nomVilleSelectionnee) {
+  		for (i=0; i<l; i++) {
+  		if (citis[i].id == nomVilleSelectionnee) {
       // garder l'element visible -> ELEMENT.style.display= "none";
    		 } else {
       // cacher l'element -> ELEMENT.style.display= "";
