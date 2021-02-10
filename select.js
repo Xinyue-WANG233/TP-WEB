eccaf2b3548404667bb2413a83aa964726bb47f1
Xinyue-WANG233 <78864878+Xinyue-WANG233@users.noreply.github.com>
2021-02-10 19:46:15 +0100

    Update select.js

--- select.js
+++ select.js
@@ -3,7 +3,7 @@
 	        var cities = document.getElementsByClassName("city");
 
   		for (i=0; i<l; i++) {
-  		if (citis[i].id == nomVilleSelectionnee) {
+  		if (cities[i].id == nomVilleSelectionnee) {
       // garder l'element visible -> ELEMENT.style.display= "none";
    		 } else {
       // cacher l'element -> ELEMENT.style.display= "";
